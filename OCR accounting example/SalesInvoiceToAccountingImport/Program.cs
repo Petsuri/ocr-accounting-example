09c1288b3775273f80f19b7822ec4af1823abe16
Petri Miikki <miikinpetri@gmail.com>
2021-02-25 19:46:09 +0200

    Added logic that handles pdf conversions to csv as a command line operation

--- OCR accounting example/SalesInvoiceToAccountingImport/Program.cs
+++ OCR accounting example/SalesInvoiceToAccountingImport/Program.cs
@@ -1,4 +1,8 @@
-﻿using SalesInvoiceImport.IronOcr;
+﻿using Accounting;
+using AccountingEntryImport.CsvHelper;
+using SalesInvoiceImport.IronOcr;
+using Serilog;
+using Serilog.Core;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -14,13 +18,34 @@ namespace SalesInvoiceToAccountingImport
 
         private static async Task Execute()
         {
-            var test = File.ReadAllBytes("./TestData/example-sales-invoice1.pdf");
-            var invoiceReader = new IronOcrSalesInvoiceReader(
-                new IronOcr.IronTesseract(), 
-                new SalesInvoiceTotalSum(new IInvoiceTotalSum[] { new SameLineInvoiceTotalSum(), new SameHeightInvoiceTotalSum() })
+
+            var empty = File.ReadAllBytes("./TestData/empty.pdf");
+            var invoice1 = File.ReadAllBytes("./TestData/example-sales-invoice1.pdf");
+            var invoice2 = File.ReadAllBytes("./TestData/example-sales-invoice2.pdf");
+
+            var command = new SalesInvoiceToAccountingImportCommand(CreateSalesInvoiceService(), CreateLogger());
+            await command.SalesInvoicePdfToCsv("empty.pdf", empty);
+            await command.SalesInvoicePdfToCsv("example-sales-invoice1.pdf", invoice1);
+            await command.SalesInvoicePdfToCsv("example-sales-invoice2.pdf", invoice2);
+        }
+
+
+
+        private static SalesInvoicePdfService CreateSalesInvoiceService()
+        {
+            return new SalesInvoicePdfService(
+                new IronOcrSalesInvoiceReader(
+                new IronOcr.IronTesseract(),
+                new SalesInvoiceTotalSum(new IInvoiceTotalSum[] { new SameLineInvoiceTotalSum(), new SameHeightInvoiceTotalSum() })),
+                new AccountingEntryFormatCsv()
             );
-            await invoiceReader.ReadPdf(test);
-            Console.WriteLine("Hello World!");
+        }
+
+        private static Logger CreateLogger()
+        {
+            return new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
         }
     }
 }
---
+++ OCR accounting example/SalesInvoiceToAccountingImport/SalesInvoiceToAccountingImportCommand.cs
@@ -0,0 +1,31 @@
+﻿using Accounting;
+using Serilog.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesInvoiceToAccountingImport
+{
+    class SalesInvoiceToAccountingImportCommand
+    {
+        private readonly SalesInvoicePdfService pdfService;
+        private readonly Logger logger;
+
+        public SalesInvoiceToAccountingImportCommand(SalesInvoicePdfService pdfService, Logger logger)
+        {
+            this.pdfService = pdfService;
+            this.logger = logger;
+        }
+
+        public async Task SalesInvoicePdfToCsv(string pdfName, byte[] pdf)
+        {
+            var result = await pdfService.ToImportFormat(pdf);
+            if (result.IsOk)
+            {
+                logger.Information("CSV for {pdfName}: " + Environment.NewLine + "{csv}", pdfName, result.Value);
+            } else
+            {
+                logger.Error("Unable to convert {pdfName} to accounting entry: {error}", pdfName, result.Error);
+            }
+        }
+    }
+}
