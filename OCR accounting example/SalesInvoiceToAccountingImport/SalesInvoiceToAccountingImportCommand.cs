66aab44c076d124e522f1efad243be805cfcf344
Petri Miikki <miikinpetri@gmail.com>
2021-02-28 16:32:00 +0200

    Added catch if something unexpected happens so we can log it and continue processing other pdfs

--- OCR accounting example/SalesInvoiceToAccountingImport/SalesInvoiceToAccountingImportCommand.cs
+++ OCR accounting example/SalesInvoiceToAccountingImport/SalesInvoiceToAccountingImportCommand.cs
@@ -18,11 +18,23 @@ namespace SalesInvoiceToAccountingImport
 
         public async Task SalesInvoicePdfToCsv(string pdfName, byte[] pdf)
         {
+            try
+            {
+                await ToCsv(pdfName, pdf);
+            } catch(Exception ex)
+            {
+                logger.Fatal("Unable to process file: {pdfName}. Exception: {ex}", pdfName, ex.ToString());
+            }
+        }
+
+        private async Task ToCsv(string pdfName, byte[] pdf)
+        {
             var result = await pdfService.ToImportFormat(pdf);
             if (result.IsOk)
             {
                 logger.Information("CSV for {pdfName}: " + Environment.NewLine + "{csv}", pdfName, result.Value);
-            } else
+            }
+            else
             {
                 logger.Error("Unable to convert {pdfName} to accounting entry: {error}", pdfName, result.Error);
             }
